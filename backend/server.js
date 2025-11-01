fee3c5f2ef0290ac8918fd12f7158d8ad270b490
shreyarawat16 <shreyarawat363@gmail.com>
2025-11-01 20:14:22 +0530

    updated root

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,13 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./src/config/data.js"
 import bookRoute from "./src/routes/bookRoute.js";
+const path from "path";
 
 dotenv.config();
 
 
 const app = express();
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-}));
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -21,6 +19,20 @@ app.get("/", (req, res) => {
 
 app.use("/api/books", bookRoute);
 
+if(process.env.NODE_ENV=== "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+  })
+}
+
+if(process.env.NODE_ENV!== "production"){
+   app.use(cors({
+    origin: ["http://localhost:5173"],
+  
+}));
+}
 const PORT = process.env.PORT || 3000;
 
 connectDB().then(()=>{
